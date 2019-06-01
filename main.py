a0ff3c2286af639fd150c58e91d439a2ad720b68
hangockhue <healwayhappy@gmail.com>
2019-06-01 11:33:49 +0700

    add

--- main.py
+++ main.py
@@ -15,92 +15,12 @@ from keras import backend as K
 from keras.models import load_model
 from PIL import Image, ImageDraw, ImageFont
 import tensorflow as tf
+from preprocess import yolo_head, yolo_eval
 
 model_path = './model/yolo.h5'
 anchors_path = './model/yolo_anchors.txt'
 classes_path = './model/coco_classes.txt'
 
-def yolo_boxes_to_corners(box_xy, box_wh):
-    """ Chuyển đổi box predictions sang box corners """
-    
-    box_mins = box_xy - (box_wh / 2.)
-    box_maxes = box_xy + (box_wh / 2.)
-
-    return K.concatenate([
-        box_mins[..., 1:2],  # y_min
-        box_mins[..., 0:1],  # x_min
-        box_maxes[..., 1:2],  # y_max
-        box_maxes[..., 0:1]  # x_max
-    ])
-
-
-def yolo_filter_boxes(boxes, box_confidence, box_class_probs, threshold=.6):
-
-    box_scores = box_confidence * box_class_probs # box_score sẽ bằng Px nhân với tỉ lệ của 80 class
-    box_classes = K.argmax(box_scores, axis=-1) # index tại giá trị lớn nhất ở trục 80 trả về (19,19,5)
-    box_class_scores = K.max(box_scores, axis=-1) # giá trị lớn nhất ứng với trục 80 trả về mảng 80 số 
-    prediction_mask = box_class_scores >= threshold
-
-    boxes = tf.boolean_mask(boxes, prediction_mask) # sẽ trả về những giá trị ứng với true
-    scores = tf.boolean_mask(box_class_scores, prediction_mask)
-    classes = tf.boolean_mask(box_classes, prediction_mask)
-
-    return boxes, scores, classes
-
-
-def yolo_eval(yolo_outputs,image_shape, max_boxes=10, score_threshold=.6, iou_threshold=.5):
-    box_xy, box_wh, box_confidence, box_class_probs = yolo_outputs
-    boxes = yolo_boxes_to_corners(box_xy, box_wh)
-    boxes, scores, classes = yolo_filter_boxes(
-        boxes, box_confidence, box_class_probs, threshold=score_threshold)
-    height = image_shape[0]
-    width = image_shape[1]
-    image_dims = K.stack([height, width, height, width]) # xếp chồng
-    image_dims = K.reshape(image_dims, [1, 4])
-    boxes = boxes * image_dims
-    # non max suppression : xử lý giúp boxes không bị chồng chéo
-    max_boxes_tensor = K.variable(max_boxes, dtype='int32')
-    K.get_session().run(tf.variables_initializer([max_boxes_tensor]))
-    nms_index = tf.image.non_max_suppression(
-        boxes, scores, max_boxes_tensor, iou_threshold=iou_threshold)
-    boxes = K.gather(boxes, nms_index) # K.gather để chọn nms index từ boxes
-    scores = K.gather(scores, nms_index)
-    classes = K.gather(classes, nms_index)
-    
-    return boxes, scores, classes
-
-def yolo_head(feats, anchors, num_classes):
-
-    num_anchors = len(anchors) 
-
-    anchors_tensor = K.reshape(K.variable(anchors), [1,1,1, num_anchors, 2])
-
-    conv_dims = K.shape(feats)[1:3] # 19x19
-
-    conv_height_index = K.arange(0, stop=conv_dims[0])
-    conv_width_index = K.arange(0, stop=conv_dims[1])
-    conv_height_index = K.tile(conv_height_index, [conv_dims[1]])
-
-    conv_width_index = K.tile(
-        K.expand_dims(conv_width_index, 0), [conv_dims[0], 1])
-    conv_width_index = K.flatten(K.transpose(conv_width_index))
-    conv_index = K.transpose(K.stack([conv_height_index, conv_width_index]))
-    conv_index = K.reshape(conv_index, [1, conv_dims[0], conv_dims[1], 1, 2])
-    conv_index = K.cast(conv_index, K.dtype(feats))
-
-    feats = K.reshape(
-        feats, [-1, conv_dims[0], conv_dims[1], num_anchors, num_classes + 5])
-    conv_dims = K.cast(K.reshape(conv_dims, [1, 1, 1, 1, 2]), K.dtype(feats))
-
-    box_xy = K.sigmoid(feats[..., :2]) # nén giá trị chạy từ 0 -> 1
-    box_wh = K.exp(feats[..., 2:4]) # hàm e mũ x 
-    box_confidence = K.sigmoid(feats[..., 4:5]) # nén giá trị chạy từ 0 -> 1
-    box_class_probs = K.softmax(feats[..., 5:]) # hàm trung bình mũ 
-
-    box_xy = (box_xy + conv_index) / conv_dims
-    box_wh = box_wh * anchors_tensor / conv_dims
-
-    return box_xy, box_wh, box_confidence, box_class_probs
 
 with open(classes_path) as f:
     class_names = f.readlines()
